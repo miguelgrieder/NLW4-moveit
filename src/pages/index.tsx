3373dbb8f30a310642b3a7d87e010d8006d6c4da
Migue Duarte Grieder <81339737+migueldgrieder@users.noreply.github.com>
2021-10-10 17:39:49 -0300

    Aula 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+
+
 import Head from 'next/head'
 import styles from '../styles/pages/Home.module.css';
 //Pagina inicial
@@ -24,7 +27,7 @@ export default function Home() {
       </div>
 
       <div>
-
+        <ChallengeBox/>
       </div>
 
     </section>
