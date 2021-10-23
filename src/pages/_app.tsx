b65165dbce558a499a0cdea77081903a7682908c
Migue Duarte Grieder <81339737+migueldgrieder@users.noreply.github.com>
2021-10-22 23:42:32 -0300

    Oauth Google início

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,25 @@
 import { PersonalizeProvider } from '../contexts/PersonalizeContext';
 import '../styles/global.css';
+import GoogleLogin from 'react-google-login';
 //Sempre carregado e renderizado a cada nova pagina
 
 
 function MyApp({ Component, pageProps }) {
+  const responseGoogle = (response)=>{
+    console.log(response);
+    console.log(response.profileObj);
+  }
   return (
+    <GoogleLogin
+    clientId="204696438746-8og13k6e857sfg42olg8dfk68pnnt5hu.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={this.responseGoogle}
+    onFailure={this.responseGoogle}
+    cookiePolicy={'single_host_origin'}>
     <PersonalizeProvider> 
       <Component {...pageProps} />
     </PersonalizeProvider>
+    </GoogleLogin>
 )};
 
 export default MyApp
